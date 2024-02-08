db4675a7e9625642aef547d9fbbe75c96a557eec
Bertalan Takács <47786829+TakacsBertalan@users.noreply.github.com>
2024-02-08 14:30:34 -0800

    Update create_shannon_plots.py

--- create_graphs/create_shannon_plots.py
+++ create_graphs/create_shannon_plots.py
@@ -9,32 +9,38 @@ from sys import argv
 ##Make sure that your input data is tsv format and has a shannon_entropy column
 ##Or modify this script to fit your needs
 
-
 filepath = argv[1]
-
 plot_type = argv[2]
 
 metadata = pd.read_csv(filepath, sep = "\t", index_col = "id") 
 metadata = metadata.drop("#q2:types")
 
+#Change this to your variable you're interested in. It needs to be the name of one of the columns in metadata
+plot_by = "tissue_type"
+#Color the box/violin plot based on this variable. It needs to be the name of one of the columns in the metadata. It can be the same as plot_by
+color_box_by = "tissue_type"
+#Color the swarmplot points based on this variable. It needs to be the name of one of the columns in the metadata. It should be the different from color_box_by.
+#For color ideas see the QIIME2 color list at the end of the document. 
+color_swarm_by = "fixation_method"
+
+
 metadata["shannon_entropy"] = metadata.shannon_entropy.astype("float")
-metadata = metadata.sort_values(["tissue_type"])
+metadata = metadata.sort_values([plot_by])
 sns.set_style(style='darkgrid', rc={"axes.facecolor":"lightgrey"})
 my_palette = sns.color_palette("deep", desat = 0.8)
 
 def create_boxplot():
-	bx = sns.boxplot(x="tissue_type",
+	bx = sns.boxplot(x=plot_by,
 	y="shannon_entropy",
 	data=metadata,
-	hue = "tissue_type",
+	hue = color_box_by,
 	palette = "tab10")
 	plt.show()
 			   
 def create_violin_and_swarm():
-	violin = sns.violinplot(x ='tissue_type', y ='shannon_entropy', data = metadata, hue = "tissue_type", palette = my_palette)
-	swarm = sns.swarmplot(x ='tissue_type', y ='shannon_entropy', data = metadata, hue = "fixation_method", palette = {"FFPE":"#ffff00", "Fresh frozen": "#0000ff", "Other": "#f27304"}, s = 4.22)
+	violin = sns.violinplot(x = plot_by, y ='shannon_entropy', data = metadata, hue = color_box_by, palette = my_palette)
+	swarm = sns.swarmplot(x = plot_by, y ='shannon_entropy', data = metadata, hue = color_swarm_by, s = 4.22)
 	sns.move_legend(swarm, "center left", bbox_to_anchor=(1,0.5))
-	swarm.set(xlabel = "Fixation method")
 	plt.show()
 
 if plot_type == "box":
@@ -42,3 +48,31 @@ if plot_type == "box":
 else:
 	create_violin_and_swarm()
 
+
+"""
+List of QIIME2 distinct colors
+        "#ff0000",
+        "#0000ff",
+        "#f27304",
+        "#008000",
+        "#91278d",
+        "#ffff00",
+        "#7cecf4",
+        "#f49ac2",
+        "#5da09e",
+        "#6b440b",
+        "#808080",
+        "#f79679",
+        "#7da9d8",
+        "#fcc688",
+        "#80c99b",
+        "#a287bf",
+        "#fff899",
+        "#c49c6b",
+        "#c0c0c0",
+        "#ed008a",
+        "#00b6ff",
+        "#a54700",
+        "#808000",
+        "#008080",
+"""
